c95a7947c7abf7276d1eab691f5945b9fe813b1c
FMotalleb <fmotalleb@gmail.com>
2022-07-05 16:37:41 +0430

    [Test] NoEncryption test this part will test all other functionalities of library other than `Encryption Methods`

---
+++ test/no_encryption_mock.dart
@@ -0,0 +1,11 @@
+part of 'http_request_cipher_test.dart';
+
+class _NoEncryptionByteDataEncrypter extends IByteDataEncrypter {
+  @override
+  Uint8List encrypt(Uint8List data) => data;
+}
+
+class _NoEncryptionByteDataDecrypter extends IByteDataDecrypter {
+  @override
+  Uint8List decrypt(Uint8List data) => data;
+}
