c80e7e1532d1178e6d233dbc8efac80f65ae1537
FMotalleb <fmotalleb@gmail.com>
2022-07-12 10:35:14 +0430

    [Feat]: add `RSA` key tools added extension on `RSAKey`s to extract them to pem file added constractor on `RSAKey`s to load them from pem file

---
+++ lib/src/cipher_utils/rsa/rsa_key_extensions.dart
@@ -0,0 +1,177 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:pointycastle/asn1.dart';
+import 'package:pointycastle/export.dart' show RSAPrivateKey, RSAPublicKey;
+
+import '../../list_extensions.dart';
+
+const _BEGIN_PRIVATE_KEY = '-----BEGIN PRIVATE KEY-----';
+const _END_PRIVATE_KEY = '-----END PRIVATE KEY-----';
+
+const _BEGIN_PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----';
+const _END_PUBLIC_KEY = '-----END PUBLIC KEY-----';
+
+const _BEGIN_RSA_PRIVATE_KEY = '-----BEGIN RSA PRIVATE KEY-----';
+const _END_RSA_PRIVATE_KEY = '-----END RSA PRIVATE KEY-----';
+
+const _BEGIN_RSA_PUBLIC_KEY = '-----BEGIN RSA PUBLIC KEY-----';
+const _END_RSA_PUBLIC_KEY = '-----END RSA PUBLIC KEY-----';
+
+/// following extensions are from: [https://github.com/Ephenodrom/Dart-Basic-Utils#cryptoutils]
+extension PrivateKeyTools on RSAPrivateKey {
+  /// extract private key to PEMPks1 encoded string
+  String toPemPkcs1String() {
+    final version = ASN1Integer(BigInt.from(0));
+    final modulus = ASN1Integer(n);
+    final publicExponent = ASN1Integer(BigInt.parse('65537'));
+    final privateExponent = ASN1Integer(this.privateExponent);
+
+    final p = ASN1Integer(this.p);
+    final q = ASN1Integer(this.q);
+    final dP = this.privateExponent! %
+        (this.p! -
+            BigInt.from(
+              1,
+            ));
+    final exp1 = ASN1Integer(dP);
+    final dQ = this.privateExponent! %
+        (this.q! -
+            BigInt.from(
+              1,
+            ));
+    final exp2 = ASN1Integer(dQ);
+    final iQ = this.q!.modInverse(this.p!);
+    final co = ASN1Integer(iQ);
+
+    final topLevelSeq = ASN1Sequence()
+      ..add(version)
+      ..add(modulus)
+      ..add(publicExponent)
+      ..add(privateExponent)
+      ..add(p)
+      ..add(q)
+      ..add(exp1)
+      ..add(exp2)
+      ..add(co);
+    final dataBase64 = base64.encode(topLevelSeq.encode());
+
+    final chunks = dataBase64.codeUnits.sliceToPiecesOfSize(64).map(
+          String.fromCharCodes,
+        );
+    // ignore: lines_longer_than_80_chars
+    return '$_BEGIN_RSA_PRIVATE_KEY\n${chunks.join('\n')}\n$_END_RSA_PRIVATE_KEY';
+  }
+
+  /// extract private key to PEM encoded string
+  String toPemString() {
+    final version = ASN1Integer(BigInt.from(0));
+
+    final algorithmSeq = ASN1Sequence();
+    final algorithmAsn1Obj = ASN1Object.fromBytes(
+      Uint8List.fromList(
+        [
+          0x6,
+          0x9,
+          0x2a,
+          0x86,
+          0x48,
+          0x86,
+          0xf7,
+          0xd,
+          0x1,
+          0x1,
+          0x1,
+        ],
+      ),
+    );
+    final paramsAsn1Obj = ASN1Object.fromBytes(Uint8List.fromList([0x5, 0x0]));
+    algorithmSeq
+      ..add(algorithmAsn1Obj)
+      ..add(paramsAsn1Obj);
+
+    final privateKeySeq = ASN1Sequence();
+    final modulus = ASN1Integer(n);
+    final publicExponent = ASN1Integer(BigInt.parse('65537'));
+    final privateExponent = ASN1Integer(this.privateExponent);
+    final p = ASN1Integer(this.p);
+    final q = ASN1Integer(this.q);
+    final dP = this.privateExponent! % (this.p! - BigInt.from(1));
+    final exp1 = ASN1Integer(dP);
+    final dQ = this.privateExponent! % (this.q! - BigInt.from(1));
+    final exp2 = ASN1Integer(dQ);
+    final iQ = this.q!.modInverse(this.p!);
+    final co = ASN1Integer(iQ);
+
+    privateKeySeq
+      ..add(version)
+      ..add(modulus)
+      ..add(publicExponent)
+      ..add(privateExponent)
+      ..add(p)
+      ..add(q)
+      ..add(exp1)
+      ..add(exp2)
+      ..add(co);
+    final publicKeySeqOctetString = ASN1OctetString(
+      octets: Uint8List.fromList(
+        privateKeySeq.encode(),
+      ),
+    );
+
+    final topLevelSeq = ASN1Sequence()
+      ..add(version)
+      ..add(algorithmSeq)
+      ..add(publicKeySeqOctetString);
+    final dataBase64 = base64.encode(topLevelSeq.encode());
+    final chunks = dataBase64.codeUnits.sliceToPiecesOfSize(64).map(
+          String.fromCharCodes,
+        );
+    return '$_BEGIN_PRIVATE_KEY\n${chunks.join('\n')}\n$_END_PRIVATE_KEY';
+  }
+}
+
+extension PublicKeyTools on RSAPublicKey {
+  /// extract public key to PEMPks1 encoded string
+  String toPemPkcs1String() {
+    final topLevelSeq = ASN1Sequence()
+      ..add(ASN1Integer(modulus))
+      ..add(ASN1Integer(exponent));
+
+    final dataBase64 = base64.encode(topLevelSeq.encode());
+    final chunks = dataBase64.codeUnits.sliceToPiecesOfSize(64).map(
+          String.fromCharCodes,
+        );
+    return '$_BEGIN_RSA_PUBLIC_KEY\n${chunks.join('\n')}\n$_END_RSA_PUBLIC_KEY';
+  }
+
+  /// extract public key to PEM encoded string
+  String toPemString() {
+    final algorithmSeq = ASN1Sequence();
+    final paramsAsn1Obj = ASN1Object.fromBytes(Uint8List.fromList([0x5, 0x0]));
+    algorithmSeq
+      ..add(ASN1ObjectIdentifier.fromName('rsaEncryption'))
+      ..add(paramsAsn1Obj);
+
+    final publicKeySeq = ASN1Sequence()
+      ..add(ASN1Integer(modulus))
+      ..add(ASN1Integer(exponent));
+    final publicKeySeqBitString = ASN1BitString(
+      stringValues: Uint8List.fromList(
+        publicKeySeq.encode(),
+      ),
+    );
+
+    final topLevelSeq = ASN1Sequence()
+      ..add(algorithmSeq)
+      ..add(publicKeySeqBitString);
+    final dataBase64 = base64.encode(topLevelSeq.encode());
+    final chunks = dataBase64.codeUnits.sliceToPiecesOfSize(64).map(
+          String.fromCharCodes,
+        );
+
+    return '$_BEGIN_PUBLIC_KEY\n${chunks.join('\n')}\n$_END_PUBLIC_KEY';
+  }
+}
--- lib/src/cipher_utils/rsa/rsa_tools.dart
+++ lib/src/cipher_utils/rsa/rsa_tools.dart
@@ -1,3 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:convert' show LineSplitter, base64Decode;
+import 'dart:typed_data' show Uint8List;
+
+import 'package:pointycastle/asn1.dart' //
+    show
+        ASN1BitString,
+        ASN1Integer,
+        ASN1Parser,
+        ASN1Sequence;
 import 'package:pointycastle/export.dart'
     show
         AsymmetricKeyPair,
@@ -8,11 +19,13 @@ import 'package:pointycastle/export.dart'
         RSAPublicKey,
         SecureRandom;
 
-abstract class RSATools {
-  RSATools._();
+/// following toolkit will allow you to generate RSA keys or load them from PEM
+/// encoded strings.
+abstract class RSAKeyTools {
+  RSAKeyTools._();
   static AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateRSAkeyPair(
     SecureRandom secureRandom, {
-    int bitLength = 256,
+    int bitLength = 2048,
   }) {
     final keyGen = RSAKeyGenerator()
       ..init(
@@ -30,7 +43,166 @@ abstract class RSATools {
 
     final pubKey = pair.publicKey as RSAPublicKey;
     final privateKey = pair.privateKey as RSAPrivateKey;
-
     return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(pubKey, privateKey);
+  }
+
+  /// load RSA (Public/Private) keys from **PEM Encoded String** (not file path).
+  static AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> loadKeyPair({
+    required String privateKey,
+    required String publicKey,
+  }) {
+    final pubKey = rsaPublicKeyFromPem(publicKey);
+    final privKey = rsaPrivateKeyFromPem(privateKey);
+    return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(
+      pubKey,
+      privKey,
+    );
+  }
+
+  /// load RSA (Public/Private) keys from **PEMPks1 Encoded String** (not file path).
+  static AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> loadKeyPairPks1({
+    required String privateKey,
+    required String publicKey,
+  }) {
+    final pubKey = rsaPublicKeyFromPemPkcs1(publicKey);
+    final privKey = rsaPrivateKeyFromPemPkcs1(privateKey);
+    return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(
+      pubKey,
+      privKey,
+    );
+  }
+
+  static RSAPrivateKey rsaPrivateKeyFromPem(String pem) {
+    final bytes = _getBytesFromPEMString(pem);
+    return _rsaPrivateKeyFromDERBytes(bytes);
+  }
+
+  static RSAPrivateKey rsaPrivateKeyFromPemPkcs1(String pem) {
+    final bytes = _getBytesFromPEMString(pem);
+    return _rsaPrivateKeyFromDERBytesPkcs1(bytes);
+  }
+
+  static RSAPublicKey rsaPublicKeyFromPem(String pem) {
+    final bytes = _getBytesFromPEMString(pem);
+    return _rsaPublicKeyFromDERBytes(bytes);
+  }
+
+  static RSAPublicKey rsaPublicKeyFromPemPkcs1(String pem) {
+    final bytes = _getBytesFromPEMString(pem);
+    return _rsaPublicKeyFromDERBytesPkcs1(bytes);
+  }
+
+  static Uint8List _getBytesFromPEMString(
+    String pem, {
+    bool checkHeader = true,
+  }) {
+    final lines = LineSplitter.split(pem)
+        .map((line) => line.trim())
+        .where(
+          (line) => line.isNotEmpty,
+        )
+        .toList();
+    String base64;
+    if (checkHeader) {
+      if (lines.length < 2 ||
+          !lines.first.startsWith(
+            '-----BEGIN',
+          ) ||
+          !lines.last.startsWith(
+            '-----END',
+          )) {
+        throw ArgumentError(
+          'The given string does not have the correct '
+          'begin/end markers expected in a PEM file.',
+        );
+      }
+      base64 = lines.sublist(1, lines.length - 1).join();
+    } else {
+      base64 = lines.join();
+    }
+
+    return Uint8List.fromList(base64Decode(base64));
+  }
+
+  static RSAPrivateKey _rsaPrivateKeyFromDERBytesPkcs1(Uint8List bytes) {
+    final asn1Parser = ASN1Parser(bytes);
+    final pkSeq = asn1Parser.nextObject() as ASN1Sequence;
+
+    final modulus = pkSeq.elements![1] as ASN1Integer;
+    final privateExponent = pkSeq.elements![3] as ASN1Integer;
+    final p = pkSeq.elements![4] as ASN1Integer;
+    final q = pkSeq.elements![5] as ASN1Integer;
+
+    final rsaPrivateKey = RSAPrivateKey(
+      modulus.integer!,
+      privateExponent.integer!,
+      p.integer,
+      q.integer,
+    );
+
+    return rsaPrivateKey;
+  }
+
+  static RSAPrivateKey _rsaPrivateKeyFromDERBytes(Uint8List bytes) {
+    var asn1Parser = ASN1Parser(bytes);
+    final topLevelSeq = asn1Parser.nextObject() as ASN1Sequence;
+    final privateKey = topLevelSeq.elements![2];
+
+    asn1Parser = ASN1Parser(privateKey.valueBytes);
+    final pkSeq = asn1Parser.nextObject() as ASN1Sequence;
+
+    final modulus = pkSeq.elements![1] as ASN1Integer;
+    final privateExponent = pkSeq.elements![3] as ASN1Integer;
+    final p = pkSeq.elements![4] as ASN1Integer;
+    final q = pkSeq.elements![5] as ASN1Integer;
+
+    final rsaPrivateKey = RSAPrivateKey(
+      modulus.integer!,
+      privateExponent.integer!,
+      p.integer,
+      q.integer,
+    );
+
+    return rsaPrivateKey;
+  }
+
+  ///
+  /// Decode the given [bytes] into an [RSAPublicKey].
+  ///
+  static RSAPublicKey _rsaPublicKeyFromDERBytes(Uint8List bytes) {
+    final asn1Parser = ASN1Parser(bytes);
+    final topLevelSeq = asn1Parser.nextObject() as ASN1Sequence;
+    ASN1Sequence publicKeySeq;
+    if (topLevelSeq.elements![1].runtimeType == ASN1BitString) {
+      final publicKeyBitString = topLevelSeq.elements![1] as ASN1BitString;
+
+      final publicKeyAsn = ASN1Parser(
+        publicKeyBitString.stringValues as Uint8List?,
+      );
+      publicKeySeq = publicKeyAsn.nextObject() as ASN1Sequence;
+    } else {
+      publicKeySeq = topLevelSeq;
+    }
+    final modulus = publicKeySeq.elements![0] as ASN1Integer;
+    final exponent = publicKeySeq.elements![1] as ASN1Integer;
+
+    final rsaPublicKey = RSAPublicKey(modulus.integer!, exponent.integer!);
+
+    return rsaPublicKey;
+  }
+
+  ///
+  /// Decode the given [bytes] into an [RSAPublicKey].
+  ///
+  /// The [bytes] need to follow the the pkcs1 standard
+  ///
+  static RSAPublicKey _rsaPublicKeyFromDERBytesPkcs1(Uint8List bytes) {
+    final publicKeyAsn = ASN1Parser(bytes);
+    final publicKeySeq = publicKeyAsn.nextObject() as ASN1Sequence;
+    final modulus = publicKeySeq.elements![0] as ASN1Integer;
+    final exponent = publicKeySeq.elements![1] as ASN1Integer;
+
+    final rsaPublicKey = RSAPublicKey(modulus.integer!, exponent.integer!);
+    return rsaPublicKey;
   }
 }
