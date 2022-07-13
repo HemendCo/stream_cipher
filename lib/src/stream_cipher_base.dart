3eced1418585031b69f1a1ff59c1873c8dda1929
FMotalleb <fmotalleb@gmail.com>
2022-07-13 14:17:21 +0430

    [Doc]

--- lib/src/stream_cipher_base.dart
+++ lib/src/stream_cipher_base.dart
@@ -1,3 +1,5 @@
+library stream_cipher.base_types;
+
 import 'dart:typed_data' show Uint8List;
 
 /// abstract of the Decrypter model
@@ -8,6 +10,8 @@ abstract class IByteDataDecrypter {
   /// the method that receives a normal [Uint8List] then
   /// returns an encrypted [Uint8List]
   Uint8List decrypt(Uint8List data);
+
+  /// reset decrypter engine
   Future<void> reset() async {}
 }
 
@@ -24,5 +28,7 @@ abstract class IByteDataEncrypter {
   Uint8List encrypt(
     Uint8List data,
   );
+
+  /// reset encrypter engine
   Future<void> reset() async {}
 }
