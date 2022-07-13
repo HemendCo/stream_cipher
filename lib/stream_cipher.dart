3d8a3130cb0d8bef567ac1524fa57df5db794054
FMotalleb <fmotalleb@gmail.com>
2022-07-13 14:18:13 +0430

    [Doc]

--- lib/stream_cipher.dart
+++ lib/stream_cipher.dart
@@ -1,9 +1,19 @@
+///
+/// This is the main entry point to the stream cipher library API.
+///
+/// It includes the following libraries:
+/// * **decrypter.dart** = models used for decryption
+/// * **encrypter.dart** = models used for encryption
+/// * **./extensions/.** = useful extensions
+/// * **base.dart** = interface of en/decryption models
+/// * **models.dart** = helper models like meta data
+///
 library stream_cipher;
 
 export 'src/byte_data_decrypter.dart';
 export 'src/byte_data_encrypter.dart';
+export 'src/extensions/list_extensions.dart';
 export 'src/extensions/stream_decrypter.dart';
 export 'src/extensions/stream_encrypter.dart';
-export 'src/helper_types.dart';
-export 'src/list_extensions.dart';
+export 'src/helper_models.dart';
 export 'src/stream_cipher_base.dart';
